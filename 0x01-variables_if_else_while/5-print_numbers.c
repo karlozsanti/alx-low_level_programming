0bcbc261033e867decaa54c289f4b7357ac9e283
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-09 16:34:11 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
- /**
-   * main - Entry point
-   *
-   * Return: Always 0 (Success)
-   */
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
-{ 
+{
 	int num2 = 0;
 
 	for (num2 = 0; num2 <= 9; num2++)
