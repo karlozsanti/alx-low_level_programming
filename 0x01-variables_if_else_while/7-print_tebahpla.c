462bb9f7be4b359283846d286940d4c45885cc37
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-09 16:40:56 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char zeus = 'z';
+
+	for (zeus = 'z'; zeus >= 'a'; zeus--)
+		putchar(zeus);
+
+	putchar('\n');
+
+	return (0);
