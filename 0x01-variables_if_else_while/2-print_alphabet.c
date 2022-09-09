19c56b5f12bc6a48902de14ffdf8ef6d09ff6cbc
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-09 15:03:33 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char zeus = 'a';
+
+	for (zeus = 'a'; zeus <= 'z'; zeus++)
+		putchar(zeus);
+
+	putchar('\n');
+
+	return (0);
+}
