3423fcda22dadd1850d5348cae4d6afe1425338a
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-13 04:36:44 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.h";
 
 /**
  * main - Entry point
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n'');
-	return(0);
+	_putchar('\n');
+	return (0);
 }
