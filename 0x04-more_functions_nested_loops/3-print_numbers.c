4f7769a44319bc82ed94789cdba1621cc89024df
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-15 05:36:52 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
  * print_numbers - prints numbers
  *
  */
+
 void print_numbers(void)
 {
 	int x;
 
-	for (x = '0'; x <= '9'; x++)
-	{
+	for (x = 0; x < 9; x++)
+
 		_putchar(x);
-	}
 	_putchar('\n');
 }
