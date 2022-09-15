09d03a28abaced2ec057e79422c9fb686d9b592e
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-15 06:58:21 -0700

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int_putcha(char c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int yeat);
+
+#endif
