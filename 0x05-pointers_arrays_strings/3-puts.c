e060815bfed47af19bad6c812c37c9d703092928
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-19 14:14:41 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "2-strlen.c"
 /**
  * _puts - puts a string
  *
@@ -7,9 +6,7 @@
  */
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; *(str + i) != '\0'; i++)
+	for (; *str != '\0'; str++)
 	{
 		_putchar(*str);
 	}
