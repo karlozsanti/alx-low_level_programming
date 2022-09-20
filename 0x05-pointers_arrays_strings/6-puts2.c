927213a528ce5de426e4d48d511cb521475c3265
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-19 21:23:32 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - puts string twice
+ *
+ * @str: string to put twice
+ */
+void puts2(char * str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
