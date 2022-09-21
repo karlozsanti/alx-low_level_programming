bfeddbb6dfb0fcf458aaa0c315340387a34ecf27
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-21 00:15:46 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reversion of  an array
+ *
+ * @a: array to be reversed
+ * @n: size of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		j = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = t;
+	}
+}
