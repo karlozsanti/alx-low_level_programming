8542c14c5fc47e44a0c105385ffaba8942ee06cc
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-22 02:04:16 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
  * @x: string param
  * Return: capitalized version of string
  */
-cahr *cap_string(char *x)
+char *cap_string(char *x)
 {
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	int len = 13;
