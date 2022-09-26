d7601f5ab5fa0ff8efd98c8a1cd163c3fde708da
Karlozsanti <onyewekepatrick@gmail.com>
2022-09-26 11:27:30 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - board to print
+ * @a: prints things
+ * Return: returns crap
+ */
+void print_chessboard(char (*a)[8])
+{
+	int k;
+	int l;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (l = 0; l < 8; l++)
+		{
+			_putchar(a[k][l]);
+		}
+		_putchar('\n');
+	}
+}
